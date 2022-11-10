a96e74282cd1d094ae4099213237e78476fd4134
kylehue <apeuda69@gmail.com>
2022-11-10 22:36:35 +0800

    add a util for fetching npm packages

--- src/utils.js
+++ src/utils.js
@@ -17,3 +17,48 @@ export function trim(str) {
   const regex = new RegExp(`^\\s{${padLength}}`);
   return lines.map(line => line.replace(regex, "")).join("\n");
 }
+
+export function isExternal(url) {
+  var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
+  if (typeof match[1] === "string" && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
+  if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":(" + { "http:": 80, "https:": 443 } [location.protocol] + ")?$"), "") !== location.host) return true;
+  return false;
+}
+
+import * as path from "path";
+import untar from "js-untar";
+import pako from "pako";
+export function getDependency(name, version) {
+  return new Promise((resolve, reject) => {
+    try {
+      const host = "registry.npmjs.org";
+      const targetURL = "https://" + path.join(host, name);
+      fetch(targetURL)
+        .then(response => response.text()).catch(error => {
+          reject(error);
+        })
+        .then(pkg => {
+          let pkgJSON = JSON.parse(pkg);
+          let pkgDist = pkgJSON["dist-tags"][version];
+          let tgzURL = pkgJSON.versions[pkgDist]?.dist?.tarball;
+
+          if (tgzURL) {
+            fetch(tgzURL)
+              .then(response => response.arrayBuffer())
+              .then(pako.inflate)
+              .then(array => array.buffer)
+              .then(untar)
+              .then(files => {
+                resolve(files);
+              });
+          } else {
+            reject("Package not found.");
+          }
+        }).catch(error => {
+          reject(error);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
