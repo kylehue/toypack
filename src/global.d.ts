faa82b0d21b8117041f0dba9c33acdc823725f7a
kylehue <apeuda69@gmail.com>
2023-01-08 21:59:20 +0800

    remove unnecessary dependencies

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,3 @@
-declare module "posthtml-parser";
-declare module "@toypack/resolve";
 declare module "postcss-value-parser";
-declare module "autoprefixer";
 declare module "merge-source-map";
-declare module "@jridgewell/sourcemap-codec";
 declare module "babel-minify";
