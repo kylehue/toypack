16fedfa09c3de6b021f173bb50742b3dda7e20c7
kylehue <apeuda69@gmail.com>
2023-04-03 23:53:32 +0800

    replace identifiers only

--- src/plugins/DefinePlugin.ts
+++ src/plugins/DefinePlugin.ts
@@ -2,13 +2,33 @@ import Toypack from "@toypack/core/Toypack";
 import { ToypackPlugin } from "@toypack/core/types";
 
 export default class DefinePlugin implements ToypackPlugin {
-   constructor(public options?: Object) {}
+   constructor(public options?: Record<string, string>) {}
 
    apply(bundler: Toypack) {
-      bundler.hooks.afterCompile((descriptor) => {
-         if (this.options) {
-            for (let [target, replacement] of Object.entries(this.options)) {
-               descriptor.compilation.content?.replaceAll(target, replacement.toString());
+      bundler.hooks.parse((descriptor) => {
+         if (typeof this.options != "object" || !this.options) {
+            return;
+         }
+
+         let replacementKeys = Object.keys(this.options);
+
+         if (!replacementKeys.length) return;
+
+         let asset = descriptor.asset;
+         let metadata = asset.loaderData.parse?.metadata;
+
+         if (/\.([jt]sx?|[cm]js)$/.test(asset.source)) {
+            if (metadata.AST) {
+               bundler._getASTImports(metadata.AST, {
+                  traverse: {
+                     Identifier: (path) => {
+                        let node = path.node;
+                        if (replacementKeys.includes(node.name)) {
+                           node.name = this.options![node.name].toString();
+                        }
+                     },
+                  },
+               });
             }
          }
       });
