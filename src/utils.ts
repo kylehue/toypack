d174b7a3df1bda486b6052a5406e0437392bb8ba
kylehue <apeuda69@gmail.com>
2023-06-14 22:53:13 +0800

    refactor(utils): remove unused utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-import path from "path-browserify";
 import {
    RawSourceMap,
    SourceMapConsumer,
@@ -6,61 +5,25 @@ import {
 } from "source-map-js";
 
 export function isLocal(source: string) {
-   return (
-      /^(?:\.\.?(?:\/|$)|\/|([A-Za-z]:)?[/\\])/.test(source) && !isURL(source)
-   );
+   if (
+      source.startsWith("./") ||
+      source.startsWith("../") ||
+      source.startsWith("/")
+   ) {
+      return !isURL(source);
+   }
+
+   return false;
 }
 
 export function isNodeModule(source: string) {
    return /^\/?node_modules/.test(source);
 }
 
-export function isChunk(source: string) {
-   return new RegExp(".chunk-[a-zA-Z0-9]+.[a-zA-Z]+$").test(source);
-}
-
-export function createChunkSource(
-   from: string,
-   lang: string,
-   id: string | number
-) {
-   return `${from}.chunk-${getHash(from + id)}.${lang}`;
-}
-
 const URL_RE = /https?:\/\/((?:[\w\d-]+\.)+[\w\d]{2,})/i;
 const DATA_URL_RE = /^(data:)([\w\/\+-]*)(;charset=[\w-]+|;base64){0,1},(.*)/gi;
 export function isURL(str: string) {
    return URL_RE.test(str) || DATA_URL_RE.test(str);
-}
-
-export function isJS(source: string) {
-   return [".js", ".mjs", ".cjs", ".jsx", ".ts", ".tsx"].includes(
-      path.extname(source)
-   );
-}
-
-export function isCSS(source: string) {
-   return [".css"].includes(path.extname(source));
-}
-
-export function formatPath(from: string, to: string) {
-   const parsed = path.parse(from);
-   let result = to;
-   for (let [property, value] of Object.entries(parsed)) {
-      if (!to.includes(`[${property}]`)) continue;
-      const propertyStr = `\\[${property}\\]`;
-      const propertyRegex = new RegExp(propertyStr, "g");
-
-      if (property == "root" || property == "dir") {
-         value = `/${value}/`;
-      }
-
-      result = result.replace(propertyRegex, value);
-   }
-
-   result = path.join(result);
-
-   return result;
 }
 
 /**
@@ -70,7 +33,7 @@ export function parseURL(url: string) {
    const result = {
       target: "",
       params: {} as Record<string, string | boolean>,
-      query: ""
+      query: "",
    };
 
    const [target, queryString] = url.split("?");
@@ -90,7 +53,7 @@ export function parseURL(url: string) {
     * Sort the params so that when we construct the query,
     * it will be the same as other queries with same value
     * but in different order.
-    * 
+    *
     * This is to avoid asset duplication when they are requested
     * with same queries but in different order.
     */
@@ -111,7 +74,7 @@ export function parseURL(url: string) {
             return 0;
          }
       });
-   
+
    result.query = sortedParams
       .map(([key, value]) => {
          if (value === true) {
@@ -121,11 +84,11 @@ export function parseURL(url: string) {
          return `${key}=${value}`;
       })
       .join("&");
-   
+
    if (result.query) {
       result.query = "?" + result.query;
    }
-   
+
    return result;
 }
 
@@ -149,30 +112,6 @@ export function getHash(str: string): string {
    }
    const uniqueId = (hash >>> 0).toString(16); // Convert to positive hexadecimal
    return "m" + uniqueId;
-}
-
-/**
- * Encodes the given content to base64 format.
- */
-export function btoa(content: string | ArrayBuffer) {
-   if (typeof window !== "undefined" && typeof window.btoa === "function") {
-      if (content instanceof ArrayBuffer) {
-         return window.btoa(
-            new Uint8Array(content).reduce(
-               (data, byte) => data + String.fromCharCode(byte),
-               ""
-            )
-         );
-      }
-
-      return window.btoa(unescape(encodeURIComponent(content)));
-   } else {
-      if (content instanceof ArrayBuffer) {
-         return Buffer.from(new Uint8Array(content)).toString("base64");
-      }
-
-      return Buffer.from(content, "utf-8").toString("base64");
-   }
 }
 
 /**
@@ -226,9 +165,6 @@ export function indexToPosition(content: string, index: number) {
    return { line, column };
 }
 
-/**
- * Simple object check.
- */
 function isObject(item: any) {
    return item && typeof item === "object" && !Array.isArray(item);
 }
