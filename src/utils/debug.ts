b39e323f81384e180f9f17e7d5d4815eef98d5eb
kylehue <apeuda69@gmail.com>
2023-06-24 18:15:12 +0800

    fix: reverse conditions

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,19 +1,19 @@
 import { LogLevelConfig } from "../types";
 
 export function error(logLevel: LogLevelConfig, message: string) {
-   if (logLevel == "error") {
+   if (logLevel == "error" || logLevel == "warn" || logLevel == "info") {
       console.error(message);
    }
 }
 
 export function warn(logLevel: LogLevelConfig, message: string) {
-   if (logLevel == "error" || logLevel == "warn") {
+   if (logLevel == "warn" || logLevel == "info") {
       console.warn(message);
    }
 }
 
 export function info(logLevel: LogLevelConfig, message: string) {
-   if (logLevel == "error" || logLevel == "warn" || logLevel == "info") {
+   if (logLevel == "info") {
       console.info(message);
    }
 }
