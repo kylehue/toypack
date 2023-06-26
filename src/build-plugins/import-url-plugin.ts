a8cb19c769f9348248104c837d727b31f5944b04
kylehue <apeuda69@gmail.com>
2023-06-26 22:26:23 +0800

    fix: support for css

--- src/build-plugins/import-url-plugin.ts
+++ src/build-plugins/import-url-plugin.ts
@@ -1,5 +1,8 @@
+import { RawSourceMap } from "source-map-js";
+import { resolve, getType, getExtension } from "../package-manager/utils.js";
 import { Plugin } from "../types.js";
-import { isUrl } from "../utils";
+import { getSourceMapUrl, isUrl, removeSourceMapUrl } from "../utils";
+import path from "path-browserify";
 
 const importUrlPlugin: Plugin = () => {
    const urls: Record<string, string> = {};
@@ -7,7 +10,7 @@ const importUrlPlugin: Plugin = () => {
       name: "import-url-plugin",
       resolve(id) {
          if (isUrl(id)) {
-            const virtualId = `virtual:${id}.js`;
+            const virtualId = `virtual:${id}`;
             urls[virtualId] = id;
             return virtualId;
          }
@@ -16,10 +19,32 @@ const importUrlPlugin: Plugin = () => {
          async: true,
          async handler(dep) {
             if (dep.source in urls) {
-               const response = await fetch(urls[dep.source]);
+               const url = urls[dep.source];
+               const response = await fetch(url);
                const content = await response.text();
+               let map: RawSourceMap | null = null;
                
-               return content;
+               if (!!this.bundler.getConfig().bundle.sourceMap) {
+                  const mapUrl = getSourceMapUrl(content);
+                  if (mapUrl) {
+                     const root = new URL(url).origin + "/";
+                     const sourceMap = await fetch(resolve(mapUrl, url, root));
+                     if (sourceMap.ok) {
+                        map = await sourceMap.json();
+                     }
+                  }
+               }
+
+               const type = getType(path.extname(url), response);
+               if (!type) {
+                  this.warn(`Could not determine the type of ${url}.`);
+               }
+
+               return {
+                  type: type || "script",
+                  content: removeSourceMapUrl(content),
+                  map
+               };
             }
          },
       },
