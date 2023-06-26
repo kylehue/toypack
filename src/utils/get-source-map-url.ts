7b600365c3a62af66f745eb0688a893aff9afa25
kylehue <apeuda69@gmail.com>
2023-06-26 22:24:30 +0800

    fix: support for multiline comment source maps

--- src/utils/get-source-map-url.ts
+++ src/utils/get-source-map-url.ts
@@ -1,12 +1,28 @@
 /**
  * Get the source map url of a content.
  */
+const singleLineRegex = /\/\/# *sourceMappingURL=(.+) *$/;
+const multiLineRegex = /\/\* *# *sourceMappingURL=(.+) *\*\/$/;
 export function getSourceMapUrl(content: string) {
-   const regex = /\/\/[#@]\s*sourceMappingURL=(.+)\s*$/gm;
-   const matches = regex.exec(content);
-   if (matches && matches.length > 1) {
-      return matches[1].trim();
+   const singleLineMatches = singleLineRegex.exec(content);
+   if (singleLineMatches && singleLineMatches.length > 1) {
+      return singleLineMatches[1].trim();
+   }
+
+   const multiLineMatches = multiLineRegex.exec(content);
+   if (multiLineMatches && multiLineMatches.length > 1) {
+      return multiLineMatches[1].trim();
    }
 
    return null;
+}
+
+/**
+ * Remove the sourceMappingUrl comment.
+ */
+export function removeSourceMapUrl(content: string) {
+   return content
+      .replace(singleLineRegex, "")
+      .replace(multiLineRegex, "")
+      .trimEnd();
 }
