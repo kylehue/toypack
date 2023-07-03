601876a1cb9d25f30dfb685cce87e441e31de199
kylehue <apeuda69@gmail.com>
2023-07-04 05:41:22 +0800

    fix: allow unbundled urls

--- src/build-plugins/import-url-plugin.ts
+++ src/build-plugins/import-url-plugin.ts
@@ -1,41 +1,183 @@
 import { RawSourceMap } from "source-map-js";
+import MapConverter from "convert-source-map";
+import generateScript from "@babel/generator";
+import { generate as generateStyle } from "css-tree";
 import { resolve, getType } from "../package-manager/utils";
-import { Plugin } from "../types.js";
-import { getSourceMapUrl, isUrl, removeSourceMapUrl } from "../utils";
+import { Plugin, Toypack } from "../types.js";
+import { isUrl, mergeSourceMaps } from "../utils";
+import { parseScriptAsset } from "../graph/parse-script-chunk";
+import { parseStyleAsset } from "../graph/parse-style-chunk";
+import { CSSTreeGeneratedResult } from "../bundle/compile-style";
+import { fetchSourceMapInContent } from "../package-manager/fetch-source-map";
+interface AssetBase {
+   url: string;
+   source: string;
+}
+interface ScriptAsset extends AssetBase {
+   type: "script";
+   content: string;
+   map?: RawSourceMap | null;
+}
+
+interface StyleAsset extends AssetBase {
+   type: "style";
+   content: string;
+   map?: RawSourceMap | null;
+}
+
+interface ResourceAsset extends AssetBase {
+   type: "resource";
+   content: Blob;
+}
+
+type Asset = ScriptAsset | StyleAsset | ResourceAsset;
+
+interface CacheData {
+   response?: Response;
+   asset?: Asset;
+}
+
+const cache = new Map<string, CacheData>();
+async function fetchUrl(this: Toypack, entryUrl: string) {
+   const config = this.getConfig();
+   const shouldMap = !!config.bundle.sourceMap;
+   let assets: Record<string, Asset> = {};
+   const recurse = async (url: string) => {
+      if (assets[url]) return;
+      let cached = cache.get(url);
+      if (!cached) {
+         cached = {};
+         cache.set(url, cached);
+      }
+
+      const response = cached?.response ? cached.response : await fetch(url);
+      cached.response ??= response;
+
+      const source = url;
+      const type = getType(response);
+      if (type == "resource") {
+         cached.asset ??= assets[url] = {
+            type: "resource",
+            content: await response.blob(),
+            url,
+            source,
+         };
+
+         return true;
+      }
+
+      const asset = {
+         type,
+         url,
+         source,
+      } as ScriptAsset | StyleAsset;
+      cached.asset ??= asset;
+      assets[url] = asset;
+
+      const rawContent = await response.text();
+      let dependencies: string[] = [];
+
+      if (type == "script") {
+         const parsedScript = await parseScriptAsset.call(
+            this,
+            source,
+            rawContent,
+            {
+               inspectDependencies(node) {
+                  const resolved = resolve(node.value, url);
+                  node.value = resolved;
+               },
+            }
+         );
+
+         dependencies = parsedScript.dependencies;
+
+         const generated = generateScript(parsedScript.ast, {
+            sourceFileName: source,
+            filename: source,
+            sourceMaps: shouldMap,
+            comments: false,
+         });
+
+         asset.content = generated.code;
+         asset.map = generated.map as any;
+      } else {
+         const parsedStyle = await parseStyleAsset.call(
+            this,
+            source,
+            rawContent,
+            {
+               inspectDependencies(node) {
+                  const resolved = resolve(node.value, url);
+                  node.value = resolved;
+               },
+            }
+         );
+
+         dependencies = parsedStyle.dependencies;
+
+         const generated = generateStyle(parsedStyle.ast, {
+            sourceMap: shouldMap,
+         }) as any as CSSTreeGeneratedResult;
+
+         if (typeof generated == "string") {
+            asset.content = generated;
+         } else {
+            asset.content = generated.css;
+            asset.map = MapConverter.fromJSON(
+               generated.map.toString()
+            ).toObject();
+         }
+      }
+
+      // Get source map
+      if (shouldMap) {
+         let sourceMap =
+            cached?.asset.type != "resource" && cached.asset.map
+               ? cached.asset.map
+               : await fetchSourceMapInContent(rawContent, url);
+
+         if (sourceMap && asset.map) {
+            asset.map = mergeSourceMaps(sourceMap, asset.map);
+         } else {
+            asset.map ||= sourceMap;
+         }
+      }
+
+      // Fetch dependencies recursively
+      for (const depSource of dependencies) {
+         const resolved = resolve(depSource, url);
+
+         const isSuccess = await recurse(resolved);
+         // break if a dependency fails
+         if (!isSuccess) return false;
+      }
+
+      return true;
+   };
+
+   await recurse(entryUrl);
+
+   return assets;
+}
 
 export default function (): Plugin {
+   let fetchedAssets: Record<string, Asset> = {};
    return {
       name: "import-url-plugin",
       load: {
          async: true,
          async handler(dep) {
-            if (isUrl(dep.source)) {
-               const url = dep.source;
-               const response = await fetch(url);
-               const content = await response.text();
-               let map: RawSourceMap | null = null;
-
-               if (!!this.bundler.getConfig().bundle.sourceMap) {
-                  const mapUrl = getSourceMapUrl(content);
-                  if (mapUrl) {
-                     const root = new URL(url).origin + "/";
-                     const sourceMap = await fetch(resolve(mapUrl, url, root));
-                     if (sourceMap.ok) {
-                        map = await sourceMap.json();
-                     }
-                  }
-               }
-
-               const type = getType(response);
-               if (!type) {
-                  this.warn(`Could not determine the type of ${url}.`);
-               }
-
-               return {
-                  type: type || "script",
-                  content: removeSourceMapUrl(content),
-                  map,
-               };
+            if (!isUrl(dep.source)) return;
+            if (!(dep.source in fetchedAssets)) {
+               Object.assign(
+                  fetchedAssets,
+                  await fetchUrl.call(this.bundler, dep.source)
+               );
+            }
+            const asset = fetchedAssets[dep.source];
+            if (asset) {
+               return asset;
             }
          },
       },
