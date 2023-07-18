c0310b7d3a05037601164e0411e7a116c23f2dfb
kylehue <apeuda69@gmail.com>
2023-07-18 21:39:00 +0800

    fix order

--- src/bundle-script/utils/get-sorted-scripts.ts
+++ src/bundle-script/utils/get-sorted-scripts.ts
@@ -9,10 +9,10 @@ export function getSortedScripts(graph: DependencyGraph) {
       .filter((g): g is ScriptDependency => g.type == "script")
       .sort((a, b) => {
          if (a.importers[b.source] && !b.importers[a.source]) {
-            return 1;
-         } else if (b.importers[a.source] && !a.importers[b.source]) {
             return -1;
+         } else if (b.importers[a.source] && !a.importers[b.source]) {
+            return 1;
          }
          return 0;
-      });
+      }).reverse();
 }
