08b59cc74e7c91202d30a2bb4353fcc9ec23c8fd
kylehue <apeuda69@gmail.com>
2023-08-01 00:40:21 +0800

    fix: node_modules should be on top

--- src/bundle-script/utils/get-sorted-modules.ts
+++ src/bundle-script/utils/get-sorted-modules.ts
@@ -1,7 +1,8 @@
 import { DependencyGraph, ScriptDependency } from "src/parse";
+import { isNodeModule } from "../../utils";
 
 /**
- * Gets the sorted script modules in the dependency graph. 
+ * Gets the sorted script modules in the dependency graph.
  * It sorts the modules by import hierarchy.
  */
 export function getSortedScripts(graph: DependencyGraph) {
@@ -11,6 +12,11 @@ export function getSortedScripts(graph: DependencyGraph) {
          if (a.importers[b.source] && !b.importers[a.source]) {
             return 1;
          } else if (b.importers[a.source] && !a.importers[b.source]) {
+            return -1;
+         }
+         if (isNodeModule(a.source) && !isNodeModule(b.source)) {
+            return 1;
+         } else if (isNodeModule(b.source) && !isNodeModule(a.source)) {
             return -1;
          }
          return 0;
