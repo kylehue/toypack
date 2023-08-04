31f94fd9cbb38027e80c54d4c3a25d2b7df993a6
kylehue <apeuda69@gmail.com>
2023-08-04 10:16:19 +0800

    fix: uid generation

--- src/utils/UidGenerator.ts
+++ src/utils/UidGenerator.ts
@@ -1,16 +1,25 @@
 import { Binding, Scope } from "@babel/traverse";
 import { camelCase } from "lodash-es";
+import { reservedWords } from "./reserved-words";
+
+function removeLeadingNums(str: string) {
+   if (!/^[0-9]/.test(str)) return str;
+   const split = str.split(/^[0-9]+/);
+   return split[split.length - 1];
+}
 
 export class UidGenerator {
    private _idCountMap: Record<string, number> = {};
-   private _reservedVars = new Set<string>();
-   private generate(name = "temp") {
+   private _reservedVars = new Set<string>([...reservedWords]);
+   public generate(name = "temp") {
+      name = removeLeadingNums(name);
       name = camelCase(name);
       let generated = name;
-      this._idCountMap[generated] ??= 0;
-      if (this._idCountMap[generated] >= 0) {
-         generated = name + "_" + this._idCountMap[generated]++;
+      this._idCountMap[name] ??= -1;
+      if (this._idCountMap[name] >= 0) {
+         generated = name + "_" + this._idCountMap[name];
       }
+      this._idCountMap[name]++;
 
       return generated;
    }
@@ -23,14 +32,22 @@ export class UidGenerator {
       this._reservedVars = new Set([...this._reservedVars, ...vars]);
    }
 
-   public generateBasedOnScope(scope: Scope, name?: string, binding?: Binding) {
+   public generateBasedOnScope(scope: Scope, name = "temp", binding?: Binding) {
       let generated = this.generate(name);
 
-      const isTaken = () =>
-         scope.hasBinding(generated) ||
-         this.isConflicted(generated) ||
-         !!binding?.referencePaths.find((x) => x.scope.hasBinding(generated));
-      
+      const isTaken = () => {
+         const _binding = scope.getBinding(generated);
+         const hasBinding =
+            (!binding && !!_binding && generated !== name) ||
+            (!!binding && !!_binding && binding !== _binding);
+         const isConflicted = this.isConflicted(generated);
+         const isConflictedInOtherScope = !![
+            ...(binding?.referencePaths || []),
+            ...(binding?.constantViolations || []),
+         ].find((x) => x.scope.hasBinding(generated));
+         return hasBinding || isConflicted || isConflictedInOtherScope;
+      };
+
       while (isTaken()) {
          generated = this.generate(name);
       }
@@ -42,6 +59,6 @@ export class UidGenerator {
 
    public reset() {
       this._idCountMap = {};
-      this._reservedVars = new Set();
+      this._reservedVars = new Set([...reservedWords]);
    }
 }
