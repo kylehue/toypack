980c0775b0079f2ada3ff886a63659d09e47bd24
kylehue <apeuda69@gmail.com>
2023-08-17 11:11:12 +0800

    feat: include `.txt` files

--- src/build-plugins/raw-plugin.ts
+++ src/build-plugins/raw-plugin.ts
@@ -4,10 +4,18 @@ import { parseURL } from "../utils";
 export default function (): Plugin {
    return {
       name: "raw-plugin",
+      extensions: [["script", ".txt"]],
       load: {
          chaining: false,
          handler(moduleInfo) {
-            if (parseURL(moduleInfo.source).params.raw !== true) return;
+            const parsedRequest = parseURL(moduleInfo.source);
+            if (
+               parsedRequest.params.raw !== true &&
+               !/\.txt$/.test(parsedRequest.target)
+            ) {
+               return;
+            }
+
             if (typeof moduleInfo.content != "string") {
                this.emitError("Blob contents are not supported.");
                return;
