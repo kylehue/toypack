2cc8713c43f7d69e2e52787e74ccb60731fdb1fd
kylehue <apeuda69@gmail.com>
2023-08-20 15:37:55 +0800

    fix: add test for blob urls

--- src/utils/is-url.ts
+++ src/utils/is-url.ts
@@ -1,6 +1,6 @@
 const URL_RE = /https?:\/\/((?:[\w\d-]+\.)+[\w\d]{2,})/i;
 const DATA_URL_RE = /data:([-\w]+\/[-+\w.]+)?(;?\w+=[-\w]+)*(;base64)?,.*/i;
-
+const BLOB_URL_RE = /^blob:\w+:\/\/\w+/i;
 /**
  * Check if string is an external url.
  * @param str The string to check.
@@ -8,7 +8,7 @@ const DATA_URL_RE = /data:([-\w]+\/[-+\w.]+)?(;?\w+=[-\w]+)*(;base64)?,.*/i;
  */
 export function isUrl(str: string) {
    if (str.startsWith("virtual:")) return false;
-   return URL_RE.test(str) || isDataUrl(str);
+   return URL_RE.test(str) || isDataUrl(str) || isBlobUrl(str);
 }
 
 /**
@@ -18,4 +18,13 @@ export function isUrl(str: string) {
  */
 export function isDataUrl(str: string) {
    return DATA_URL_RE.test(str);
+}
+
+/**
+ * Check if string is a blob url.
+ * @param str The string to check.
+ * @returns A boolean.
+ */
+export function isBlobUrl(str: string) {
+   return BLOB_URL_RE.test(str);
 }
