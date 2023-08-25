e9bd5cf983b6cf2ba57ae482d5a94b73596856b0
kylehue <apeuda69@gmail.com>
2023-08-25 19:51:16 +0800

    docs: clean urls

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
    title: "Toypack",
    description: "The sandbox bundler for static sites.",
+   cleanUrls: true,
    themeConfig: {
       // https://vitepress.dev/reference/default-theme-config
       nav: [
